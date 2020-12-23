8aed4f45c52a6e4f92a39cb92dd382fa3faa2bfb
esraras <eesraras29@gmail.com>
2020-12-23 21:28:17 +0300

    added account kit

--- app/src/main/java/com/example/projedemo/Welcome.java
+++ app/src/main/java/com/example/projedemo/Welcome.java
@@ -1,13 +1,29 @@
 package com.example.projedemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Welcome extends AppCompatActivity {
+import com.huawei.agconnect.auth.AGConnectAuth;
 
+public class Welcome extends AppCompatActivity implements View.OnClickListener {
 
+    Button singUp;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
+
